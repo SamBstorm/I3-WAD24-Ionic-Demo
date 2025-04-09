d8b4e852d2ab0f7354d53ed3218ebbabb76ce821
Samuel Legrain <samuel.legrain@bstorm.be>
2025-04-09 13:32:24 +0200

    Add Demo-Timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,29 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  public finalTime!: Date;
+  public time: number;
+  public finalHour : string;
 
-  constructor() {}
+  constructor() {
+    this.finalHour = formatDate(new Date(),'hh:mm','en');
+    this.setFinalTime();
+    this.time = this.getDelta();
+    setInterval(
+      () => this.time = this.getDelta(),
+      1000
+    );
+  }
 
+  public getDelta() : number{
+    let now : number = new Date().getTime();
+    return Math.floor((this.finalTime.getTime() - now) / 1000);
+  }
+
+  public setFinalTime() : void{
+    this.finalTime = new Date();
+    this.finalTime.setHours(parseInt(this.finalHour.substring(0,2)));
+    this.finalTime.setMinutes(parseInt(this.finalHour.substring(3,5)));
+    this.finalTime.setSeconds(0);
+  }
 }
