64ff07be1cea252f69194bc94102cc1a0759ecd8
Samuel Legrain <samuel.legrain@bstorm.be>
2025-04-09 14:02:00 +0200

    Add ion-content

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-profil',
@@ -8,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
+  @ViewChild(IonContent) content! : IonContent
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public scrollToBottom(){
+    this.content.scrollToBottom(500);
+  }
+
+  public scrollToTop(){
+    this.content.scrollToTop(1500);
+  }
+
+  public startToScroll() : void{
+    console.log("Démarrage du scroll!");
+  }
+
+  public duringScrolling(event : any) : void{
+    if(event) console.log(event);
+    else console.log("Weeeeeeee!");
+  }
+  
+  public endOfScroll() : void{
+    console.log("Fin du scroll!");
   }
 
 }
